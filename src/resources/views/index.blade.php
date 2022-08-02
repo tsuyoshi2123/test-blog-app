d5b00cd77ae1eb665664405623ea0e50863b1813
髙橋剛志 <tt2123244@gmail.com>
2022-08-03 06:42:19 +0900

    ADD : 一覧画面作成

---
+++ src/resources/views/index.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>index_view</title>
+</head>
+<body>
+    <div class="header-top">
+        <h2 class="header-title">BLOG APP</h2>
+    </div>
+
+    <div class="index-wrapper">
+        <h3 class="index-title">飯記録</h3>
+        <div class="index-content">
+            <div class="index-content-columns">
+                <p>ID</p>
+                <p>名前</p>
+                <p>食べたご飯</p>
+                <p>コメント</p>
+                <p>日付</p>
+            </div>
+            <div class="index-column-values">
+                <p>1</p>
+                <p>山田太郎</p>
+                <p>オムライス</p>
+                <p>おいちかった</p>
+                <p>1996/07070</p>
+                <p class="edit">編集</p>
+                <a class="delete">削除</a>
+            </div>
+            <div class="index-column-values">
+                <p>1</p>
+                <p>山田太郎</p>
+                <p>オムライス</p>
+                <p>おいちかった</p>
+                <p>1996/07070</p>
+                <p class="edit">編集</p>
+                <a class="delete">削除</a>
+            </div>
+            <div class="index-column-values">
+                <p>1</p>
+                <p>山田太郎</p>
+                <p>オムライス</p>
+                <p>おいちかった</p>
+                <p>1996/07070</p>
+                <p class="edit">編集</p>
+                <a class="delete">削除</a>
+            </div>
+
+        </div>
+    </div>
+</body>
+</html>
