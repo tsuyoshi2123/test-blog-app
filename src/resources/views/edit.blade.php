e1471d8b0cbbde9b2f2f5a8e7973c845e2df21ba
髙橋剛志 <tt2123244@gmail.com>
2022-08-30 09:31:34 +0900

    style : edit 側修正

--- src/resources/views/edit.blade.php
+++ src/resources/views/edit.blade.php
@@ -14,28 +14,55 @@
     </div>
     <h2 class="create-title">メッシ投稿編集</h2>
     <div class="create-wrapper">
-        <div class="create-box">
-            <p class="name">名前</p>
-            {!! Form::input('text', 'name', $blog->name, ['id' => 'create-name', 'form' => 'create-new']) !!}
+        <div class="create-box @if($errors->has('name')) has-error @endif">
+            <div class="create-box-content">
+                <p class="name">名前</p>
+                {!! Form::input('text', 'name', null, ['id' => 'create-name', 'form' => 'create-new']) !!}
+            </div>
+            @if($errors->has('name'))
+            @foreach($errors->get('name') as $error)
+            <p class="error-message">{{ $error }}</p>
+            @endforeach
+            @endif
         </div>
-        <div class="create-box">
-            <p class="eate">食べたご飯</p>
-            {!! Form::input('text', 'food', $blog->food, ['id' => 'create-eate', 'form' => 'create-new']) !!}
+        <div class="create-box @if($errors->has('food')) has-error @endif">
+            <div class="create-box-content">
+                <p class="food">食べたご飯</p>
+                {!! Form::input('text', 'food', null, ['id' => 'create-name', 'form' => 'create-new']) !!}
+            </div>
+            @if($errors->has('food'))
+            @foreach($errors->get('food') as $error)
+            <p class="error-message">{{ $error }}</p>
+            @endforeach
+            @endif
         </div>
-        <div class="create-box">
-            <p class="comment">コメント</p>
-            {!! Form::input('text', 'comment', $blog->comment, ['id' => 'create-comment', 'form' => 'create-new']) !!}
+        <div class="create-box @if($errors->has('comment')) has-error @endif">
+            <div class="create-box-content">
+                <p class="comment">コメント</p>
+                {!! Form::input('text', 'comment', null, ['id' => 'create-name', 'form' => 'create-new']) !!}
+            </div>
+            @if($errors->has('comment'))
+            @foreach($errors->get('comment') as $error)
+            <p class="error-message">{{ $error }}</p>
+            @endforeach
+            @endif
         </div>
-        <div class="create-box">
-            <p class="time">日付</p>
-            {!! Form::date('day', $blog->time, ['id'=>'create-time', 'name' => 'time', 'form' => 'create-new']) !!}
+        <div class="create-box @if($errors->has('time')) has-error @endif">
+            <div class="create-box-content">
+                <p class="time">日付</p>
+                {!! Form::date('day', Carbon::now(), ['id'=>'create-time', 'name' => 'time', 'form' => 'create-new']) !!}
+            </div>
+            @if($errors->has('time'))
+            @foreach($errors->get('time') as $error)
+            <p class="error-message">{{ $error }}</p>
+            @endforeach
+            @endif
         </div>
         <div class="btn-box">
             {!! Form::open(['route' => 'blog.index' , 'method' => 'GET']) !!}
                 @csrf
                 {!! Form::submit('戻る', ['class' => 'index-btn']) !!}
             {!! Form::close() !!}
-            {{-- {!! Form::open(['route' => ['report.update', $dailyReport->id], 'method' => 'PUT']) !!} --}}
             {!! Form::open(['route' => ['blog.update', $blog->id], 'method' => 'PUT', 'id' => 'create-new']) !!}
                 {!! Form::submit('更新', ['class' => 'edit-btn']) !!}
             {!! Form::close() !!}
